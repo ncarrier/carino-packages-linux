af512ad9f4dec75ae117be69edb055801dfb6cb8
Nicolas Carrier <nicolas.carrier@parrot.com>
2015-11-22 17:17:05 +0100

    export input-event-codes.h

--- atom.mk
+++ atom.mk
@@ -8,5 +8,8 @@ LOCAL_CATEGORY_PATH := system
 
 LOCAL_LINUX_MAKE_BUILD_ARGS:= LOADADDR=0x40008000
 
+LINUX_EXPORTED_HEADERS := \
+	$(LOCAL_PATH)/include/uapi/linux/input-event-codes.h
+
 include $(BUILD_LINUX)
 
