fdd8327e6ca90f4cfc402d5c236f035de88ae0dc
Nicolas Carrier <nicolas.carrier@parrot.com>
2015-11-25 07:16:23 +0100

    [BUILD] LOADADDR is now passed with TARGET_LINUX_MAKE_BUILD_ARGS

--- atom.mk
+++ atom.mk
@@ -6,8 +6,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := linux
 LOCAL_CATEGORY_PATH := system
 
-LOCAL_LINUX_MAKE_BUILD_ARGS:= LOADADDR=0x40008000
-
 LINUX_EXPORTED_HEADERS := \
 	$(LOCAL_PATH)/include/uapi/linux/input-event-codes.h
 
